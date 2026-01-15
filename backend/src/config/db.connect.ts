3e12a3d397c3b5bfa57ef46107c3544b3e3d0789
BelattiMateo <mateobelatti7@gmail.com>
2026-01-15 18:29:01 -0300

    feat: configuracion de sequalize y singleton para MySQL

---
+++ backend/src/config/db.connect.ts
@@ -0,0 +1,36 @@
+import { config } from "dotenv";
+import { Sequelize } from "sequelize";
+
+config();
+const dbPassword: string | null = process.env.DB_PASSWORD || null;
+const dbUser: string | null = process.env.DB_USER || null
+
+export class DataBase {
+
+    private static instance: Sequelize | null = null;
+
+    private constructor() {}
+
+    private static async connectDB() : Promise<Sequelize>{
+            if (!dbPassword || !dbUser) {
+                throw new Error("Error en Usuario o Conraseña de DB");
+            }
+            const sequelize = new Sequelize('canela_artesanias', dbUser, dbPassword, {
+                host: 'localhost', // o tu host
+                dialect: 'mysql', // 'mysql', 'sqlite', 'mssql', etc. [3]
+            });
+
+            await sequelize.authenticate();
+            return sequelize;
+    }
+
+    public static async getInstance() : Promise<Sequelize>{
+        if (!this.instance) {
+            this.instance = await DataBase.connectDB();
+        } 
+        return this.instance;
+    }
+}
+
+
+

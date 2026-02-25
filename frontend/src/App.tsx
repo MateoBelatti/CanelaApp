e3b3da8101c41d225c6966f3c47a16db205de3ce
MateoBelatti <mateobelatti7@gmail.com>
2026-02-25 11:59:57 -0300

    chore(frontend): configuracion base de proyecto y setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,25 @@
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import  Header  from "./components/layouts/header";
+import Footer from './components/layouts/footer';
+import HomePage from './pages/homePage';
+import ContactPage from './pages/contactoPage';
+import ProductPage from './pages/productPage';
+
 function App() {
   
   return (
-    <>
-      
-    </>
+    <Router>
+      <Header />
+      <main>
+        {/* RUTAS */}
+        <Routes> 
+          <Route path="/" element={<HomePage />} />
+          <Route path="/contacto" element={<ContactPage />} />
+          <Route path="/producto" element={<ProductPage />} />
+        </Routes>
+      </main>
+      <Footer />
+    </Router>
   )
 }
 

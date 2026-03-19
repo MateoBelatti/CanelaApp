793731a7caf419d1c5f4e5b739f4d36a182f2911
BelattiMateo <mateobelatti7@gmail.com>
2026-03-19 13:39:36 -0300

    refactor: elimino paginas de productos y servicios sin utilizar

--- frontend/src/pages/homePage.tsx
+++ frontend/src/pages/homePage.tsx
@@ -1,10 +1,12 @@
 import HeroSection  from "../components/home/heroSeccion";
+import ProductsSeccion from "../components/home/productsSeccion";
 import { AboutMe } from "../components/home/aboutMe";
 
 const HomePage : React.FC  = () => {
     return (
         <>
         <HeroSection/>
+        <ProductsSeccion />
         <AboutMe />
         </>
     )

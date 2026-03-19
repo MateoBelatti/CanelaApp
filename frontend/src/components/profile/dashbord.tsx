14f734ddda133c2641cd499885d635b8eff37d92
BelattiMateo <mateobelatti7@gmail.com>
2026-03-19 13:41:02 -0300

    feat(ui): actualizo page home, componentes de perfil y estilos

---
+++ frontend/src/components/profile/dashbord.tsx
@@ -0,0 +1,11 @@
+import type React from "react";
+import "../../styles/profile/dashbord.css";
+
+export const Dashbord : React.FC = ( )  => {
+    return (
+        <div className="dashbord-page">
+            { /* SECCION DE PEDIDODS RECIBIDOS */ }
+            { /* DASHBORD´S DE VENTAS */ }
+        </div>
+    )
+}
